1ec2edd6e9fc5051a6537db4a09c4b831eb683de
Yuliia <julija.neshta@ro.ru>
2021-12-03 11:20:35 +0200

    add tablet adapt

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -2,10 +2,12 @@ import '../styles/main.scss';
 
 const menuBtn = document.querySelector('.header__burger');
 const header = document.querySelector('.header');
+const menu = document.querySelector('.menu');
 
 menuBtn.addEventListener('click', () => {
   menuBtn.classList.toggle('js-open');
   header.classList.toggle('js-open');
+  menu.classList.toggle('js-open');
 });
 
 const menuItem = document.querySelectorAll('.menu__dd-item');
