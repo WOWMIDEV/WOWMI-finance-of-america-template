bf93332171cfdd2060a476e731ee671320105b8a
Kirill Sakun <kirillsakun03@gmail.com>
2021-12-07 18:59:33 +0300

    [quiz] functionality added

--- src/pages/quiz-page/quiz-page.js
+++ src/pages/quiz-page/quiz-page.js
@@ -9,25 +9,52 @@ const quizDropdowns = new Dropdowns({
   toggleSelector: '.faq__question',
   contentSelector: '.faq__content',
   activeClass: 'js-open',
+  initialDropdowns: [0],
 });
 
 quizDropdowns.init();
 
-const previewAnswer = document.querySelectorAll('.faq-quiz__answer-preview');
-const faqQuizItem = document.querySelector('.quiz__item');
-// const btnNextQuestion = document.querySelectorAll('.btn-next');
-// const disabledItem = document.querySelectorAll('.faq-quiz__block');
-const valueRadioBtn = document.querySelectorAll('[data-question = "1"] .faq-quiz__radio');
-
-
-// получить все значения радио кнопок
-// заблокировать вопрос, если не выбран предыдущий
+// const previewAnswer = document.querySelectorAll('.faq-quiz__answer-preview');
+// const faqQuizItem = document.querySelectorAll('.quiz__item');
+// // const btnNextQuestion = document.querySelectorAll('.btn-next');
+// // const disabledItem = document.querySelectorAll('.faq-quiz__block');
+// const valueRadioBtn = document.querySelectorAll('[data-question] .faq-quiz__radio');
 //
+//
+// // получить все значения радио кнопок
+// // заблокировать вопрос, если не выбран предыдущий
+// //
+//
+// valueRadioBtn.forEach((radio, i) => {
+//   radio.addEventListener('change', () => {
+//     console.log(radio.value);
+//     previewAnswer.textContent = radio.value;
+//     faqQuizItem[i].classList.add('js--filled');
+//   });
+// });
 
-valueRadioBtn.forEach((radio) => {
-  radio.addEventListener('change', () => {
-    console.log(radio.value);
-    previewAnswer.textContent = radio.value;
-    faqQuizItem.classList.add('js-fill');
+(function initQuiz() {
+  const radioButtons = document.querySelectorAll('.faq-quiz__radio');
+  const nextButtons = document.querySelectorAll('.btn-next');
+
+  radioButtons.forEach((radio) => {
+    radio.addEventListener('change', () => {
+      const quizStep = radio.closest('.quiz__item');
+      const answerPreview = quizStep.querySelector('.faq-quiz__answer-preview');
+      const nextButton = quizStep.querySelector('.btn-next');
+      const labelText = radio.nextElementSibling.textContent;
+
+      quizStep.classList.add('js--filled');
+      answerPreview.textContent = labelText;
+      nextButton.removeAttribute('disabled');
+    });
   });
-});
+
+  nextButtons.forEach((button, i) => {
+    button.addEventListener('click', () => {
+      quizDropdowns.open(i + 1);
+    });
+  });
+
+  console.log([...radioButtons].filter((r) => r.checked));
+}());
