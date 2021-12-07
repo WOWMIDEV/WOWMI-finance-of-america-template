84293d9652235d4b27f30195b761cf066d9b7424
Yuliia <julija.neshta@ro.ru>
2021-12-07 22:05:36 +0200

    add new build

--- imagemin.config.js
+++ imagemin.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   gifsicle: { optimizationLevel: 2, interlaced: false, colors: 10 },
-  mozjpeg: { progressive: true, quality: 10 },
-  pngquant: { quality: [0.25, 0.5] },
+  mozjpeg: { progressive: true, quality: 60 },
+  pngquant: { quality: [0.55, 0.8] },
   svgo: {
     plugins: [
       { removeViewBox: false },
