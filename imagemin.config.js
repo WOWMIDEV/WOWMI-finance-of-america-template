5d35ddc91b2527048792088aabd1fd47db9c4841
Yuliia <julija.neshta@ro.ru>
2021-12-23 16:34:52 +0200

    add new carousel

--- imagemin.config.js
+++ imagemin.config.js
@@ -1,12 +1,9 @@
 module.exports = {
   gifsicle: { optimizationLevel: 2, interlaced: false, colors: 10 },
-  mozjpeg: { progressive: true, quality: 90 },
+  mozjpeg: { progressive: true, quality: 100 },
   pngquant: { quality: [1, 1] },
   svgo: {
-    plugins: [
-      { removeViewBox: false },
-      { cleanupIDs: true },
-    ],
+    plugins: [{ removeViewBox: false }, { cleanupIDs: true }],
   },
   webp: { quality: 10 },
 };
--- src/pages/module-carousel/module-carousel.js
+++ src/pages/module-carousel/module-carousel.js
@@ -8,28 +8,26 @@ import 'swiper/swiper.min.css';
 Swiper.use([Navigation, Pagination]);
 // eslint-disable-next-line no-unused-vars
 const swiper = new Swiper('.carousel', {
-  spaceBetween: 112,
-  centeredSlides: true,
-  loop: true,
+  spaceBetween: 40,
   slidesPerView: 'auto',
-  loopedSlides: 1,
 
   breakpoints: {
     // when window width is >= 320px
-    320: {
+    // 320: {
+    //   slidesPerView: 1,
+    //   spaceBetween: 20,
+    // },
+    810: {
       slidesPerView: 1,
-      spaceBetween: 20,
-    },
-    570: {
-      spaceBetween: 40,
-      slidesPerView: 'auto',
-    },
-    1024: {
-      spaceBetween: 112,
-      centeredSlides: true,
       loop: true,
-      loopedSlides: 1,
+      spaceBetween: 0,
     },
+    // 1024: {
+    //   spaceBetween: 112,
+    //   centeredSlides: true,
+    //   loop: true,
+    //   loopedSlides: 1,
+    // },
   },
 
   pagination: {
