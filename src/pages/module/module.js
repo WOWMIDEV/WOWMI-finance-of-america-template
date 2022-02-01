fa5aa7df1e23e4204c90f99bb76595369253a8d9
Yuliia <julija.neshta@ro.ru>
2022-02-01 10:20:28 +0200

    add new module page

--- src/pages/module/module.js
+++ src/pages/module/module.js
@@ -1,6 +1,10 @@
 import './module.scss';
 import '../../assets/scripts/main';
 
+import Swiper, { Navigation, Pagination } from 'swiper';
+import 'swiper/swiper-bundle.min.css';
+import 'swiper/swiper.min.css';
+
 import Dropdowns from '../../modules/dropdowns/dropdowns';
 
 const dropdowns = new Dropdowns({
@@ -8,3 +12,27 @@ const dropdowns = new Dropdowns({
   contentSelector: '.faq__content',
 });
 dropdowns.init();
+
+Swiper.use([Navigation, Pagination]);
+// eslint-disable-next-line no-unused-vars
+const swiper = new Swiper('.video-slider', {
+  spaceBetween: 40,
+  slidesPerView: 'auto',
+
+  breakpoints: {
+    810: {
+      slidesPerView: 1,
+      loop: false,
+      spaceBetween: 0,
+    },
+  },
+
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.video-slider__next',
+    prevEl: '.video-slider__prev',
+  },
+});
